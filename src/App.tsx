251619442d2f0345027c7892fa82554cff623c67
Ariel desde trabajo <ariegonzaguer@gmail.com>
2025-12-16 08:22:51 -0600

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import useUserStore from './store/useStore'
 
 function App() {
   const [count, setCount] = useState(0)
+  const { count: storeCount, increment, otroEstado, objetoEstado } = useUserStore()
 
   return (
     <>
@@ -18,8 +20,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <h2>Estado Local</h2>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+        </button>
+        
+        <h2>Estado Global (Store)</h2>
+        <p>Count Store: <strong>{storeCount}</strong></p>
+        <p>Otro Estado: <strong>{otroEstado}</strong></p>
+        <p>Objeto Estado: <strong>{JSON.stringify(objetoEstado)}</strong></p>
+        <button onClick={increment}>
+          Increment Store Count
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
